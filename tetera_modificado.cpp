0abfa841d79e117a1ba872edf32acd2ca0d5d198
root <root@debian>
2015-03-23 14:09:07 -0600

    subiendo archivos

--- tetera_modificado.cpp
+++ tetera_modificado.cpp
@@ -1,13 +1,11 @@
-#include <stdio.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
-
 double rotate_y=0;
 double rotate_x=0;
-double rotate_z=0;
+double rotate_z=0.2;
+GLfloat zoom=0.5;
 
-GLfloat zoom=60.0f;
 
 void init(void)
 {
@@ -18,35 +16,22 @@ glShadeModel (GL_FLAT);
 void display(void)
 {
 /* borra la pantalla */
-glClear (GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+glClear (GL_COLOR_BUFFER_BIT);
 /* seleccionamos la matriz modelo/vista */
 glMatrixMode(GL_MODELVIEW);
 /* color */
 glColor3f(1.0, 1.0, 1.0);
-
-/* transformación modelo/vista */
 glLoadIdentity();
-glRotatef(rotate_x, 1.0, 0.0, 0.0 );
-glRotatef(rotate_y, 0.0, 1.0, 0.0 );
-glRotatef(rotate_z, 0.0, 0.0, 1.0 );
-glTranslatef(0.0, 0.0, -5.0);
+glTranslatef(0.0, 0.0, 0.0);
+glRotatef(0.0, 0.0, 0.0, 1.0);
+/* transformación modelo/vista */
+gluLookAt(rotate_x,rotate_y,rotate_z, //punto de vista en el eje X,Y,Z
+0.0,0.0,0.0, //Centro del Objeto en el eje X,Y,Z
+0.0,1.0,0.0); //Orientación de la cámara (vector UP el eje X,Y,Z)
 /* Dibujamos una tetera */
-glutWireTeapot(1.0);
+glutWireTeapot(zoom);
 /* Vacia el buffer de dibujo */
 glFlush ();
-glutSwapBuffers();
-}
-void reshape(int w, int h)
-{
-/* definemos el marco */
-glViewport(0, 0, w, h);
-/* cambiamos a la matriz de proyección */
-glMatrixMode(GL_PROJECTION);
-/* definimos una proyección perspectiva con
-un fovy de 60 grados y el near a 1.0 y el
-far a 20.0 */
-glLoadIdentity();
-gluPerspective(GLfloat(zoom), GLfloat(w) / GLfloat(h), 1.0, 20.0);
 }
 
 void keyboard(unsigned char key, int x, int y)
@@ -54,37 +39,37 @@ void keyboard(unsigned char key, int x, int y)
 	switch (key)
 	
 	{
-	case 'x':
-		rotate_x +=7; 
-		break;
-	case 'X':
-			rotate_x -=7;
-			break;
-	case 'y':
-		rotate_y +=7; 
-		break;
-	case 'Y':
-			rotate_y -=7;
-			break;
 	case 'z':
-		rotate_z +=7; 
-		break;
+			rotate_z +=0.2; 
+			break;
 	case 'Z':
-			rotate_z -=7;
+			rotate_z -=0.2;
 			break;
-	
-	case '-':
-			zoom += 10.0;
-			break;
-    case '+':
-        zoom -= 10.0;
+    case 'x' :
+        rotate_x += 0.2;
         break;
-    
+    case 'X' :
+          rotate_x -= 0.2;
+        break;
+    case 'y' :
+        rotate_y += 0.2;
+        break;
+    case 'Y' :
+        rotate_y -= 0.2;
+        break;
+    	
+    case '-':
+			zoom -=0.1;
+			break;
+	case '+':
+			zoom +=0.1;
+			break;
    
 	}
 	 glutPostRedisplay();
 	
 	}
+
 
 int main(int argc, char** argv)
 {
@@ -102,12 +87,9 @@ glutCreateWindow("Dibujando una tetera 3d");
 init();
 // Indica cual es la función de dibujo
 glutDisplayFunc(display);
-glutKeyboardFunc(keyboard); //invoca funcion de teclado
-//glutSpecialFunc(specialKeys);//invoca teclas de función especial
+glutKeyboardFunc(keyboard);
 // Indica cual es la función para el cambio de tamaño de laventana
-glutReshapeFunc(reshape);
 
 // Comienza el bucle de dibujo y proceso de eventos.
 glutMainLoop();
-return 0;
 }
